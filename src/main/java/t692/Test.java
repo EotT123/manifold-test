de3ba99113fd3e7938813b85ebe5cfc470f89316
EotT123 <EotT123>
2025-04-19 16:54:05 +0200

    #692: add error description
    https://github.com/manifold-systems/manifold/issues/692

--- src/main/java/t692/Test.java
+++ src/main/java/t692/Test.java
@@ -5,7 +5,7 @@ public class Test {
     public static void main(String[] args) {
         Foo foo = new Foo();
         foo.test("bar", "foobar");
-        foo.test(bar:"bar", foobar:"foobar");
-        foo.test(foobar:"foobar");
+        foo.test(bar:"bar", foobar:"foobar"); // ERROR Required type: String Provided: manifold_tuple_xxx
+        foo.test(foobar:"foobar"); // ERROR Required type: String Provided: manifold_tuple_xxx
     }
 }
