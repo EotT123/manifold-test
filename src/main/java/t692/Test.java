cf3c5b7ceb468ae171b45b8895b340be327365b8
EotT123 <EotT123>
2025-04-19 12:27:48 +0200

    #692
    https://github.com/manifold-systems/manifold/issues/692

---
+++ src/main/java/t692/Test.java
@@ -0,0 +1,11 @@
+package t692;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+        foo.test("bar", "foobar");
+        foo.test(bar:"bar", foobar:"foobar");
+        foo.test(foobar:"foobar");
+    }
+}
