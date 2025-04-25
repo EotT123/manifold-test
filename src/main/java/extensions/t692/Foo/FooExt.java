a26696098be8fc6c41b1a70d5248b35c10aee155
EotT123 <EotT123>
2025-04-25 23:28:25 +0200

    #692 fix example

--- src/main/java/extensions/t692/Foo/FooExt.java
+++ src/main/java/extensions/t692/Foo/FooExt.java
@@ -7,7 +7,7 @@ import t692.Foo;
 @Extension
 public class FooExt {
 
-    public static String test(@This Foo foo, String bar, String foobar){
+    public static String test(@This Foo foo, String bar = "", String foobar){
         return bar + foobar;
     }
 }
