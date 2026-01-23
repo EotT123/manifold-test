f9dfb5f0351d41eb5c869b7a72567cd0e84e0d6a
EotT123 <EotT123>
2026-01-23 11:50:21 +0100

    Extra case with @ExtensionSource

--- src/main/java/ij62/Test.java
+++ src/main/java/ij62/Test.java
@@ -1,10 +1,17 @@
 package ij62;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
 public class Test {
 
-    public Test(String... values) {
+    public void arrayExtension(String... values) {
         values.stream().collect(Collectors.joining());
+    }
+
+    public void extensionSource() throws IOException {
+        // IntelliJ error: Cannot resolve method 'list' in 'Path'
+        Paths.get("").list().forEach(p -> System.out.println(p.isDirectory()));
     }
 }
