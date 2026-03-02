7df9c2df2843b1106f0f8f823244cd5b6ed3716f
EotT123 <EotT123>
2026-03-02 23:04:15 +0100

    Add more different cases

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,16 +1,25 @@
 public class Test {
 
-    public static class MyClass<T> {
-        private T t;
+  public static class MyClass<T> {
+    private T t;
 
-        public MyClass(T t=null, int i=5) {
-            this.t = t;
-        }
+    public MyClass(T t=null, int i=5) {
+      this.t = t;
     }
+  }
 
-    static void main() {
-        MyClass<String> c1 = new MyClass<>(i:3); // IntelliJ error: Incompatible equality constraint: String and Object
-        MyClass<String> c2 = new MyClass<String>(i:3); // IntelliJ error: Cannot resolve constructor 'MyClass($constructor_<Object>)'
-        MyClass<String> c3 = new MyClass<>(t:"test", i:3); // Ok when 't' parameter is provided
-    }
+  static void main() {
+    MyClass<String> c1 = new MyClass<>(i:10); // IntelliJ error: Incompatible equality constraint: String and Object: MyClass<String> and MyClass<Object>
+    MyClass<String> c2 = new MyClass<String>(i:11); // IntelliJ error: Cannot resolve constructor 'MyClass($constructor_<Object>)'
+    var c3 = new MyClass<String>(i:12); // IntelliJ error: Cannot resolve constructor 'MyClass($constructor_<Object>)'
+    MyClass<String> c4 = new MyClass<>(t:"test", i:13); // Ok when 't' parameter is provided
+    test2(new MyClass<>(i:14)); // IntelliJ error: Incompatible equality constraint: String and Object: MyClass<String> and MyClass<Object>
+  }
+
+  public static MyClass<String> test() {
+    return new MyClass<>(i:15); // IntelliJ error: Incompatible equality constraint: String and Object: MyClass<String> and MyClass<Object>
+  }
+
+  public static void test2(MyClass<String> myClass) {
+  }
 }
