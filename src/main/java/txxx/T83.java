ee4f9eaadc58f6e858b0a4d80281e5cc3b268066
EotT123 <EotT123>
2026-03-15 17:25:33 +0100

    #83-IJ-Plugin
    https://github.com/manifold-systems/manifold-ij/issues/83

---
+++ src/main/java/txxx/T83.java
@@ -0,0 +1,33 @@
+package txxx;
+
+import static manifold.ext.props.rt.api.PropOption.*;
+
+import manifold.ext.props.rt.api.override;
+import manifold.ext.props.rt.api.val;
+
+public class T83 {
+
+    public abstract static class Test {
+
+        abstract @val String foo;
+        @val(Abstract) String bar;
+    }
+
+    public static class Test2 extends Test {
+
+        @Override
+        public String getFoo() {
+            return "foo";
+        }
+
+        @Override
+        public String getBar() {
+            return "bar";
+        }
+    }
+
+    public static class Test3 extends Test {
+        @override @val String foo = "foo";
+        @override @val String bar = "bar";
+    }
+}
